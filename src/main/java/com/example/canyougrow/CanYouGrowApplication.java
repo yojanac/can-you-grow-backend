5fcf7a71ff9f52ef47dbc59c982fe95509fadf1d
Yojana <yojanac1989@gmail.com>
2026-05-19 23:43:29 +0530

    initial file addition (demo)

---
+++ src/main/java/com/example/canyougrow/CanYouGrowApplication.java
@@ -0,0 +1,13 @@
+package com.example.canyougrow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CanYouGrowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CanYouGrowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/canyougrow/CanYouGrowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.canyougrow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CanYouGrowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
